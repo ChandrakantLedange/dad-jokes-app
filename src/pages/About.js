9e63633981ebb550ba52027d09f53022699a6407
Chandrakant Ledange <chandrakant0039@gmail.com>
2021-03-07 16:44:53 +0530

    back Added

--- src/pages/About.js
+++ src/pages/About.js
@@ -2,7 +2,7 @@ import React from 'react';
 
 export default function About() {
   return (
-    <div className="container mx-auto mt-5">
+    <div className="container mx-auto mt-5 text-white">
       <h1>This is React JS Application </h1>
       <p>
         In this project we covere Rect states,Hooks,functional component and
